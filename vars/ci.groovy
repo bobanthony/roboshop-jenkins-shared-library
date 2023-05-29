679f6e13d0c6a7ae11b461b6e95ef99390bf590a
Dinesh <satyadineshk328@gmail.com>
2023-05-29 10:53:48 +0530

    j

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,24 +1,44 @@
 def call() {
+    if(env.sonar_extra_opts) {
+        sonar_extra_opts = ""
+    }
     pipeline {
         agent any
 
         stages {
             stage ('compile/build') {
                 steps {
-                   common.compile()
+                    scripts {
+                        common.compile()
+                    }
+
                 }
             }
             stage ('test cases') {
                 steps {
-                   common.testcases()
+                    scripts {
+                        common.testcases()
+                    }
+
                 }
             }
             stage ('code quality') {
                 steps {
-                    common.codequality()
+                    scripts {
+                        common.codequality()
+
+                    }
+
                 }
             }
         }
     }
 
+}
+
+post {
+    failure {
+        mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'satyadineshk328@gmail.com', subject: "${component} - Pipeline Failed", to: 'satyadineshk328@gmail.com',  mimeType: 'text/html'
+
+    }
 }
