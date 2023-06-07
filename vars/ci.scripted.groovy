c5eb806d782af7f63976fe6327241db834271aab
Dinesh <satyadineshk328@gmail.com>
2023-06-07 12:00:35 +0530

    j

--- vars/ci.scripted.groovy
+++ vars/ci.scripted.groovy
@@ -1,37 +1,53 @@
 def call() {
-    node('workstation')
-
-
-
-    try {
-        stage('checkout') {
-            cleanws()
-        }
-
-
-
-        if (env.BRANCH_NAME != 'main') {
-            stage('compile/build') {
-                common.compile
-        }
-
-        }
-
-        stage('test cases') {
-            common.testcases
-        }
-
-        stage('code quality') {
-            common.codequality
-        }
-
-    }  catch (e) {
-
-
+    if (!env.sonar_extra_opts) {
+        env.sonar_extra_opts=""
     }
 
+    if(env.TAG_NAME ==~ ".*") {
+        env.GTAG = "true"
+    } else {
+        env.GTAG = "false"
+    }
+    node('workstation') {
+
+        try {
+
+            stage('Check Out Code') {
+                cleanWs()
+                git branch: 'main', url: "https://github.com/raghudevopsb71/${component}"
+            }
+
+            if (env.BRANCH_NAME != "main") {
+                stage('Compile/Build') {
+                    common.compile()
+                }
+            }
+
+            if(env.GTAG != "true" && env.BRANCH_NAME != "main") {
+                stage('Test Cases') {
+                    common.testcases()
+                }
+            }
+
+            if (BRANCH_NAME ==~ "PR-.*"){
+                stage('Code Quality') {
+                    common.codequality()
+                }
+            }
+
+            if(env.GTAG == "true") {
+                stage('Package') {
+                    common.prepareArtifacts()
+                }
+                stage('Artifact Upload') {
+                    common.artifactUpload()
+                }
+            }
 
 
+        } catch (e) {
+            mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'raghudevopsb71@gmail.com', subject: "${component} - Pipeline Failed", to: 'raghudevopsb71@gmail.com',  mimeType: 'text/html'
+        }
 
+    }
 }
-
--- vars/common.groovy
+++ vars/common.groovy
@@ -1,20 +1,48 @@
 def compile() {
-   if(app_lang == "nodejs")
-       sh 'npm install'
-    if(app_lang == "maven")
-        sh 'mvn package'
+    if(app_lang == "nodejs") {
+        sh 'npm install'
+    }
+    if(app_lang == "maven") {
+        sh 'mvn package ; mv target/${component}-1.0.jar ${component}.jar'
+    }
 }
 
 def testcases() {
-    if(app_lang == "nodejs")
-        echo 'Run unit tests'
-    if(app_lang == "maven")
-        echo 'Run test cases'
-}
-
-static def codequality() {
-    'sonar-scanner -Dsonar.host.url=http//IP:9000 -Dsonar.login=admin -Dsonar.password=admin -Dsonar.projectkey=${component}' as java.lang.Object
+    // npm test
+    // mvn test
+    // python -m unittests
+    // go test
+    sh 'echo OK'
 }
 
 
+def codequality() {
+    withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
+        //sh 'sonar-scanner -Dsonar.host.url=http://172.31.12.89:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts} -Dsonar.qualitygate.wait=true'
+        sh 'echo OK'
+    }
 
+}
+
+def prepareArtifacts() {
+//  sh 'echo ${TAG_NAME} >VERSION'
+//  if (app_lang == "maven") {
+//    sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
+//  } else {
+//    sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
+//  }
+    sh 'docker build -t 739561048503.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
+}
+
+def artifactUpload() {
+//  env.NEXUS_USER = sh ( script: 'aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+//  env.NEXUS_PASS = sh ( script: 'aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+//  wrap([$class: 'MaskPasswordsBuildWrapper',
+//        varPasswordPairs: [[password: NEXUS_PASS],[password: NEXUS_USER]]]) {
+//    sh 'echo ${TAG_NAME} >VERSION'
+//    sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.11.210:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+//
+//  }
+    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 739561048503.dkr.ecr.us-east-1.amazonaws.com'
+    sh 'docker push 739561048503.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+}
